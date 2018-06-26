aaa3b4ff4590d8778f568a2ffc53717bae51b0d9
Adriana Rios <amrios2817@gmail.com>
2018-06-26 17:03:49 -0400

    feature(mobile-menu): add flyout menu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../../img/cat-dog-logo.jpg';
 
-const Navbar = () => (
-  <nav className="ppp--navbar" role="navigation">
-    <ol>
-      <li className="ppp--brand" role="banner">
-        <Link to='/' className="ppp--brand__link">
-          <img className="ppp--logo" src={logo} alt="line drawing of cat and dog"/>
-          <header className="ppp--header">
-            Professional Pet Petters
-          </header>
-        </Link>
-      </li>
-      <li className="ppp--menu-item"><Link to='/roster'>Roster</Link></li>
-      <li className="ppp--menu-item"><Link to='/player'>Players</Link></li>
-      <li className="ppp--menu-item"><Link to='/login'>Log In</Link></li>
-      <li className="ppp--menu-item"><Link to='/register'>Register</Link></li>
-      <li className="ppp--menu__mobile"><span></span></li>
-    </ol>
-  </nav>
-);
+export default class NavBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = { open: false };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
 
-export default Navbar
+  toggleMenu() {
+    this.setState({
+      open: !this.state.open
+    });
+  }
+
+  render() {
+    return (
+      <nav className="ppp--navbar" role="navigation">
+        <ol>
+          <li className="ppp--brand" role="banner">
+            <Link to='/' className="ppp--brand__link">
+              <img className="ppp--logo" src={logo} alt="line drawing of cat and dog"/>
+              <header className="ppp--header">
+                Professional Pet Petters
+              </header>
+            </Link>
+          </li>
+          <li className="ppp--menu-item"><Link to='/roster'>Roster</Link></li>
+          <li className="ppp--menu-item"><Link to='/player'>Players</Link></li>
+          <li className="ppp--menu-item"><Link to='/login'>Log In</Link></li>
+          <li className="ppp--menu-item"><Link to='/register'>Register</Link></li>
+
+          {/* Mobile Menu */}
+          <li className="ppp--menu__mobile" onClick={this.toggleMenu}><span></span></li>
+          {this.state.open && (<ol className="ppp--menu__mobile-open">
+            <li className="ppp--menu-item__mobile" onClick={this.toggleMenu}>
+              <Link to='/roster'>Roster</Link>
+            </li>
+            <li className="ppp--menu-item__mobile" onClick={this.toggleMenu}>
+              <Link to='/player'>Players</Link>
+            </li>
+            <li className="ppp--menu-item__mobile" onClick={this.toggleMenu}>
+              <Link to='/login'>Log In</Link>
+            </li>
+            <li className="ppp--menu-item__mobile" onClick={this.toggleMenu}>
+              <Link to='/register'>Register</Link>
+            </li>
+          </ol>)}
+        </ol>
+      </nav>
+    )
+  }
+}
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -6,27 +6,28 @@ export default class Login extends Component {
     return (
       <div className="ppp--login">
         <HomePage />
-          <fieldset className="ppp--form-field">
-            <legend><h2>Register</h2></legend>
+        <fieldset className="ppp--form-field">
+          <legend><h2>Register</h2></legend>
 
-            <label htmlFor="first_name">First Name</label>
-            <input className="ppp--text-input" name="first_name" type="text" placeholder="your first name" required />
+          <label htmlFor="first_name">First Name</label>
+          <input className="ppp--text-input" name="first_name" type="text" placeholder="your first name" required />
 
-            <label htmlFor="last_name">Last Name</label>
-            <input className="ppp--text-input" name="last_name" type="text" placeholder="your last name" required />
+          <label htmlFor="last_name">Last Name</label>
+          <input className="ppp--text-input" name="last_name" type="text" placeholder="your last name" required />
 
-            <label htmlFor="email">Email Address</label>
-            <input className="ppp--text-input" type="email" name="email" placeholder="enter your email" required />
+          <label htmlFor="email">Email Address</label>
+          <input className="ppp--text-input" type="email" name="email" placeholder="enter your email" required />
 
-            <label htmlFor="password">Password</label>
-            <input className="ppp--text-input" type="password" name="password" placeholder="enter your password" required />
+          <label htmlFor="password">Password</label>
+          <input className="ppp--text-input" type="password" name="password" placeholder="enter your password" required />
 
-            <label htmlFor="passwordConfirm">Confirm Password</label>
-            <input className="ppp--text-input" type="password" name="passwordConfirm" placeholder="confirm your password" required />
+          <label htmlFor="passwordConfirm">Confirm Password</label>
+          <input className="ppp--text-input" type="password" name="passwordConfirm" placeholder="confirm your password" required />
 
-            <button className="ppp--button">Register</button>
-            <p>Already have an account? <a href='/login'>Log in.</a></p>
-          </fieldset>
+          <button className="ppp--button">Register</button>
+          <p>Already have an account? <a href='/login'>Log in.</a></p>
+        </fieldset>
+
       </div>
     )
   }
